3038b5b212d9588b7b4c8cef032ebc0bd48247e0
positr0nix <irvin.py@gmail.com>
2017-10-12 22:03:19 -0500

    Add comments to samplefunc.py

--- samplefunc.py
+++ samplefunc.py
@@ -1,8 +1,24 @@
+# functions example
+
+# we define a function like this:
+# def funName(attrs):
+#
 def multiply(a,b):
-    c=a*b
-    print "product is %r" %c
+    # Here return a value, a function can return or not a value
+    return a*b 
+
 def add(a,b):
-    e=a+b
-    print "addition is %r" %e
-multiply(3,4)
-add(3,4)
+    # Here return a value
+    return a+b
+
+# In this function don't return any value
+def sayHello(name):
+    print "Hello ", name
+
+# we call this functions here
+print "multiply: ", multiply(3,4)
+print "add: ", add(3,4)
+
+sayHello("Python")
+
+# learn more about functions here https://www.learnpython.org/en/Functions
