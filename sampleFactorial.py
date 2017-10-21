00d9cc2e305bc6d6c7141eaab3544b9f98216231
i-m-sid <28786196+i-m-sid@users.noreply.github.com>
2017-10-21 14:45:45 +0530

    added efficiency and comments in factorial program
    factorial(n) is being called two times which will take more time.
    Instead storing it in variable ans printing it will take less time.

--- sampleFactorial.py
+++ sampleFactorial.py
@@ -1,9 +1,13 @@
-def fatorial(n):
-    if(n == 0 or n == 1):
-        return 1
-    else:
+#defining the factorial function
+def fatorial(n):                 
+    if(n == 0 or n == 1):        
+        return 1  
+    #since factorial of 1 and 0 is 1
+    else:                      
         return fatorial(n-1)*n
+    #since n! = n*(n-1)!, we call function recursively
 
-n = int(input("Please, enter the numb: "))
-fatorial(n)
-print(fatorial(n))
+n = int(input("enter a num "))    
+ans = fatorial(n)
+#calling the function 'factorial' we created above and storing the returned value in 'ans'
+print(ans)                        
